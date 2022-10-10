46474efafd8525d2c13e9abf2e7879b65e919014
DheerajNani7 <60538004+DheerajNani7@users.noreply.github.com>
2022-10-09 23:01:27 -0400

    Update News.js

--- client/src/News.js
+++ client/src/News.js
@@ -1,62 +1,74 @@
-import React, { useState } from 'react'
-import { useNavigate } from "react-router-dom";
-import axios from 'axios'
-export function News() {
-    const navigate = useNavigate();
+import React from 'react';
 
-  const [news, setNews] = useState([])
+import 'bootstrap/dist/css/bootstrap.css';
 
-  const fetchNews = () => {
-    axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=4f9f84129c964ac9a6c6687006a2cc22")
-      .then((response) => {
-        console.log(response);
-        setNews(response.data.articles)
-      })
-  }
-  return (
-    <>
-      <div className="container my-3">
-        <div className="row d-flex justify-content-center">
-          <div className="col-4">
-            {<button className='btn btn-primary' onClick={fetchNews}>Click here to Fetch the News</button>}
-          </div>
-        </div>
-      </div>
 
-      <div className="container">
-        <div className="row">
-          {
-            news.map((value,index) => {
-              return (
-                
-                      <div class="row g-0">
-        <div class=" col-md-4 d-flex justify-content-center">
-            <img src={value.urlToImage} class="img-fluid rounded-start " alt="dada" width="300px" height="300px"/>
-        </div>
-        <div class=" col-md-6 row g-4">
-            <div class="card-body">
-                <h5 class="d-flex flex-row " type="text" name="Nam" id="name">{value.title}</h5>
-                <p class="text-start">{value.description}</p>
-                
-                
-                
-       
-            </div>
-        </div>
-        <p><br/><br/></p>
-        </div>
-                  
-                
-                
-              );
-            })
-          }
-        </div>
-        <div>
-        
-        </div>
-       
-      </div>
-    </>
-  )
+export class News extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            articles : []
+        }
+    }
+    loadArticle(articleID){
+    fetch("https://community-hacker-news-v1.p.rapidapi.com/item/" + articleID+ ".json?print=pretty", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-hacker-news-v1.p.rapidapi.com",
+		"x-rapidapi-key": "240bb206f9mshca7ca9655e5cb50p1f52b9jsnb37effc43d98"
+	}
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data); 
+    
+    this.setState((prevState, props) => ({
+        articles : [...prevState.articles, data]
+    }))
+})
+.catch(err => {
+	console.log(err);
+});
+    }
+
+    loadNews(){
+    fetch("https://community-hacker-news-v1.p.rapidapi.com/topstories.json?print=pretty", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-hacker-news-v1.p.rapidapi.com",
+		"x-rapidapi-key": "240bb206f9mshca7ca9655e5cb50p1f52b9jsnb37effc43d98"
+	}
+})
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    for (let index = 0; index < 10; index++) {
+        this.loadArticle(data[index])
+        debugger;
+    }
+})
+.catch(err => {
+	console.log(err);
+});  
+
+
+    }
+    componentDidMount(){
+        this.loadNews();
+    }
+
+    render(){
+        return(
+            <div>
+                <ul class="list-group">
+                    {this.state.articles.map(article =>{
+                        return(<li class="list-group-item">   <a href='{article.url}'>{article.title}</a></li>)
+                    })}
+                </ul>
+            </div>  
+        )
+    }
 }
